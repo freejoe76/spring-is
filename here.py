c531d0f1d804620be009297d43fc57e48d51e1b3
Joe Murphy <joe.murphy@gmail.com>
2018-03-31 23:20:50 -0400

    I think it works

--- here.py
+++ here.py
@@ -15,7 +15,8 @@ class Spring:
         self.base_year = 2018 + int(self.volume)
         lines = {
             'YYYY-03-22': '''Spring Is Here
-  Taro Gomi''',
+  Taro Gomi
+  volume X''',
             'YYYY-03-25': 'Spring is here.',
             'YYYY-04-03': 'The snow melts.',
             'YYYY-04-09': 'The earth is fresh.',
@@ -53,10 +54,22 @@ class Spring:
             data = fh.write(contents)
         return True
 
-    def check_volume(self):
-        ''' Check the current volume for whether we've tweeted a particular twet.
+    def the_next_year(self, tweet):
+        ''' Update the volume and the volume tweet.
             '''
-        pass
+        self.volume += 1
+        self.file_put_contents('_volume', str(self.volume))
+        return tweet.replace('volume X', str(self.volume))
+
+    def check_for_tweet(self, date_str):
+        ''' Check and see if we're tweeting today.
+            '''
+        for key in self.lines:
+            if date_str == key:
+                if 'Taro Gomi' in self.lines[key]:
+                    return self.the_next_year(self.lines[key])
+                return self.lines[key]
+        return False
 
 def main(args):
     """ 
@@ -72,17 +85,13 @@ def main(args):
     auth = tweepy.OAuthHandler(consumer['key'], consumer['secret'])
     auth.set_access_token(access['token'], access['secret'])
     api = tweepy.API(auth)
-    #api.update_status('Spring is here.')
     s = Spring()
-    s.file_put_contents('_volume', '0')
-    t = date.today()
-    y = t.year
-    d_str = t.strftime('%Y-%m-%d')
-    
-    #print(s.lines)
-    #print(dir(api))
-    #'add_list_member', 'add_list_members', 'api_root', 'auth', 'blocks', 'blocks_ids', 'cache', 'compression', 'configuration', 'create_block', 'create_favorite', 'create_friendship', 'create_list', 'create_saved_search', 'destroy_block', 'destroy_direct_message', 'destroy_favorite', 'destroy_friendship', 'destroy_list', 'destroy_saved_search', 'destroy_status', 'direct_messages', 'favorites', 'followers', 'followers_ids', 'friends', 'friends_ids', 'friendships_incoming', 'friendships_outgoing', 'geo_id', 'geo_search', 'geo_similar_places', 'get_direct_message', 'get_list', 'get_oembed', 'get_saved_search', 'get_settings', 'get_status', 'get_user', 'home_timeline', 'host', 'list_members', 'list_subscribers', 'list_timeline', 'lists_all', 'lists_memberships', 'lists_subscriptions', 'lookup_friendships', 'lookup_users', 'me', 'media_upload', 'mentions_timeline', 'parser', 'proxy', 'rate_limit_status', 'related_results', 'remove_list_member', 'remove_list_members', 'report_spam', 'retry_count', 'retry_delay', 'retry_errors', 'retweet', 'retweeters', 'retweets', 'retweets_of_me', 'reverse_geocode', 'saved_searches', 'search', 'search_host', 'search_root', 'search_users', 'send_direct_message', 'sent_direct_messages', 'set_delivery_device', 'set_settings', 'show_friendship', 'show_list_member', 'show_list_subscriber', 'statuses_lookup', 'subscribe_list', 'suggested_categories', 'suggested_users', 'suggested_users_tweets', 'supported_languages', 'timeout', 'trends_available', 'trends_closest', 'trends_place', 'unretweet', 'unsubscribe_list', 'update_list', 'update_profile', 'update_profile_background_image', 'update_profile_banner', 'update_profile_image', 'update_status', 'update_with_media', 'upload_host', 'upload_root', 'user_timeline', 'verify_credentials', 'wait_on_rate_limit', 'wait_on_rate_limit_notify'
-    
+    #s.file_put_contents('_volume', '0')
+    d_str = date.today().strftime('%Y-%m-%d')
+    tweet = s.check_for_tweet(d_str)
+    if tweet:
+        api.update_status(tweet)
+        print("WROTE TWEET %s" % tweet)
   
 
 def build_parser(args):
@@ -91,16 +100,17 @@ def build_parser(args):
         >>> print args.verbose
         True
         """
-    parser = argparse.ArgumentParser(usage='$ python project.py',
-                                     description='What this file does.',
-                                     epilog='Example use: python project.py')
+    parser = argparse.ArgumentParser(usage='$ python here.py',
+                                     description='Sees if we should tweet, and if we should, what.',
+                                     epilog='Example use: python here.py')
     parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true")
+    parser.add_argument("-t", "--test", dest="test", default=False, action="store_true")
     args = parser.parse_args(args)
     return args
 
 if __name__ == '__main__':
     args = build_parser(sys.argv[1:])
 
-    if args.verbose == True:
+    if args.test== True:
         doctest.testmod(verbose=args.verbose)
     main(args)
