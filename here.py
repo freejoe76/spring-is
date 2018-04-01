d2edb11cf00836a5e8c2f14010489f3a6459d2d1
Joe Murphy <joe.murphy@gmail.com>
2018-03-31 23:11:13 -0400

    file read/write, new dict on each new iteration

--- here.py
+++ here.py
@@ -6,11 +6,14 @@ import random
 import doctest
 import argparse
 import tweepy
+from collections import OrderedDict
 
 class Spring:
 
     def __init__(self):
-        self.lines = {
+        self.volume = self.file_get_contents('_volume')
+        self.base_year = 2018 + int(self.volume)
+        lines = {
             'YYYY-03-22': '''Spring Is Here
   Taro Gomi''',
             'YYYY-03-25': 'Spring is here.',
@@ -30,6 +33,26 @@ class Spring:
             'YYY1-03-14': 'The calf has grown.',
             'YYY1-03-21': 'Spring is here.'
         }
+        self.lines = OrderedDict()
+        next_year = self.base_year + 1
+        for key in lines:
+            new_key = key.replace('YYYY', str(self.base_year)).replace('YYY1', str(next_year))
+            self.lines[new_key] = lines[key]
+
+    def file_get_contents(self, fn):
+        ''' As described.
+            '''
+        with open(fn) as fh:
+            data = fh.read()
+        return data
+
+    def file_put_contents(self, fn, contents):
+        ''' As described.
+            '''
+        with open(fn, 'w') as fh:
+            data = fh.write(contents)
+        return True
+
     def check_volume(self):
         ''' Check the current volume for whether we've tweeted a particular twet.
             '''
@@ -51,11 +74,12 @@ def main(args):
     api = tweepy.API(auth)
     #api.update_status('Spring is here.')
     s = Spring()
+    s.file_put_contents('_volume', '0')
     t = date.today()
     y = t.year
     d_str = t.strftime('%Y-%m-%d')
     
-    print(s.lines)
+    #print(s.lines)
     #print(dir(api))
     #'add_list_member', 'add_list_members', 'api_root', 'auth', 'blocks', 'blocks_ids', 'cache', 'compression', 'configuration', 'create_block', 'create_favorite', 'create_friendship', 'create_list', 'create_saved_search', 'destroy_block', 'destroy_direct_message', 'destroy_favorite', 'destroy_friendship', 'destroy_list', 'destroy_saved_search', 'destroy_status', 'direct_messages', 'favorites', 'followers', 'followers_ids', 'friends', 'friends_ids', 'friendships_incoming', 'friendships_outgoing', 'geo_id', 'geo_search', 'geo_similar_places', 'get_direct_message', 'get_list', 'get_oembed', 'get_saved_search', 'get_settings', 'get_status', 'get_user', 'home_timeline', 'host', 'list_members', 'list_subscribers', 'list_timeline', 'lists_all', 'lists_memberships', 'lists_subscriptions', 'lookup_friendships', 'lookup_users', 'me', 'media_upload', 'mentions_timeline', 'parser', 'proxy', 'rate_limit_status', 'related_results', 'remove_list_member', 'remove_list_members', 'report_spam', 'retry_count', 'retry_delay', 'retry_errors', 'retweet', 'retweeters', 'retweets', 'retweets_of_me', 'reverse_geocode', 'saved_searches', 'search', 'search_host', 'search_root', 'search_users', 'send_direct_message', 'sent_direct_messages', 'set_delivery_device', 'set_settings', 'show_friendship', 'show_list_member', 'show_list_subscriber', 'statuses_lookup', 'subscribe_list', 'suggested_categories', 'suggested_users', 'suggested_users_tweets', 'supported_languages', 'timeout', 'trends_available', 'trends_closest', 'trends_place', 'unretweet', 'unsubscribe_list', 'update_list', 'update_profile', 'update_profile_background_image', 'update_profile_banner', 'update_profile_image', 'update_status', 'update_with_media', 'upload_host', 'upload_root', 'user_timeline', 'verify_credentials', 'wait_on_rate_limit', 'wait_on_rate_limit_notify'
     
